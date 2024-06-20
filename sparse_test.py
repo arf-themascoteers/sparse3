a5d89b195278130eb21f9d2616a686c5710e41a5
Arif <arif@themascoteers.com>
2024-06-20 10:30:28 +1000

    done

--- sparse_test.py
+++ sparse_test.py
@@ -1,17 +1,31 @@
 import torch
+import torch.nn as nn
 import matplotlib.pyplot as plt
 
+relu = nn.ReLU()
+lrelu = nn.LeakyReLU()
+
+def custom_regularization2(weights):
+    threshold = 0.8
+    m_weights = weights-threshold
+    m_weights = relu(m_weights)
+    m_weights = torch.where(m_weights==0,0, m_weights+threshold)
+    return m_weights
 
 def custom_regularization(weights):
     threshold = 0.8
-    reg_loss = torch.where(weights < threshold, torch.abs(weights), (torch.abs(weights)-threshold)*.2+threshold)
-    return reg_loss
+    #weights = torch.where(weights<0.8,0, weights)
+    weights = torch.where(weights<0.8,weights*0.2, weights)
+    return weights
+
 
 weights = torch.linspace(-2,2,100)
 s = custom_regularization(weights)
 
 for i in range(len(weights)):
-    print(f"Weight: {weights[i]:.2f} SCAD: {s[i]:.2f}")
+    print(f"Weight: {weights[i]:.2f} LR: {s[i]:.2f}")
 
 plt.plot(weights, s)
+plt.xlim(-2,2)
+plt.ylim(-2,2)
 plt.show()
